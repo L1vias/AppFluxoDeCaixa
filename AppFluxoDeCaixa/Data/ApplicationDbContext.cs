d00e069939e649d4cce568deab3a57fe6bf7f40b
L1vias <limasenai12@gmail.com>
2024-08-28 16:42:49 -0300

    Criando a tabela clientes

--- AppFluxoDeCaixa/Data/ApplicationDbContext.cs
+++ AppFluxoDeCaixa/Data/ApplicationDbContext.cs
@@ -1,13 +1,22 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using AppFluxoDeCaixa.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace AppFluxoDeCaixa.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+        public DbSet<Clientes> Clientes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<Clientes>().ToTable("3689Clientes");
         }
     }
 }
