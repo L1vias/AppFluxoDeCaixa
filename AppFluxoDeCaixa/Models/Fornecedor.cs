46ee954a52191e6ee93444bea922d6ca5c5c6750
L1vias <limasenai12@gmail.com>
2024-09-10 14:15:50 -0300

    finalizando as views do clientes

---
+++ AppFluxoDeCaixa/Models/Fornecedor.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Identity.Client;
+using System.ComponentModel.DataAnnotations;
+
+namespace AppFluxoDeCaixa.Models
+{
+    public class Fornecedor
+    {
+        public Guid FornecedorId { get; set; }
+
+        [Display(Name = "Nome")]
+        [Required(ErrorMessage = "O Campo Nome é Obrigatório!")]
+        [MaxLength(100, ErrorMessage = "O Limite do campo é de 100 Caracteres!")]
+        [MinLength(3, ErrorMessage = "O Campo Nome deve ter, no mínimo, 3 Caracters")]
+        public string Nome { get; set; }
+
+
+        [Display(Name = "CNPJ")]
+        [Required(ErrorMessage = "O Campo é Obrigatório!")]
+        public string CNPJ { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "E-mail")]
+        [Required(ErrorMessage = "O Campo E-mail é Obrigatório!")]
+        public string Email { get; set; }
+
+        [Display(Name = "Telefone")]
+        [Required(ErrorMessage = "O Campo Nome é Obrigatório!")]
+        [MaxLength(11, ErrorMessage = "O campo Telefone deve ter, no máximo, 11 Caracteres!")]
+        [MinLength(3, ErrorMessage = "O Campo Telefone deve ter, no mínimo, 10 Caracters!")]
+        public string Telefone { get; set; }
+
+        [Display(Name = "Data de Cadastro")]
+        [Required(ErrorMessage = "O Campo Data de Cadastro é Obrigatório!")]
+        public DateTime DataDeCadastro { get; set; }
+
+        public bool Ativo { get; set; }
+    }
+}
